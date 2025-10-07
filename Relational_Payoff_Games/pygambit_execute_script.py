e84208fc50fa46474d4f66a05377141d55e370f1
LeidenschaftLiebe <197123891+LeidenschaftLiebe@users.noreply.github.com>
2025-10-07 14:24:20 +0100

    Add Entry Deterrence game efg files.

--- Relational_Payoff_Games/pygambit_execute_script.py
+++ Relational_Payoff_Games/pygambit_execute_script.py
@@ -19,7 +19,7 @@ N_VERSIONS = 20
 # Choose which builders to run (match against base name or filename).
 # Examples:
 # SELECT_BUILDERS = ["pd_builder", "builder_*", "*rps*"]
-SELECT_BUILDERS = ["stackelberg"]
+SELECT_BUILDERS = ["entry_deterrence"]
 
 # Builders to skip (applied after SELECT_BUILDERS).
 # Examples:
